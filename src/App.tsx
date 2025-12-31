cd9ca799a75b1a2b6ad2f20bc730522f53f2ed31
Christopher Forti <christopher.forti.79@gmail.com>
2025-12-30 21:05:03 -0500

    Fix MainPage import case sensitivity

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import Header from "./components/Header";
-import MainPage from "./components/mainPage";
+import MainPage from "./components/MainPage";
 import Design from "./components/Design";
 import Footer from "./components/Footer";
 
